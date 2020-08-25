a1a541809861c32cd5e037641e35e5f607742c4c
numberwolf <porschegt23@foxmail.com>
2020-08-25 22:30:46 +0800

    updateStruct

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-var TsDemuxerJs = require('./src/tsdemuxerjs-clz')
+var TsDemuxerJs = require('./src/tsdemuxerjs')
 global.makeTsDemuxerJs = (videoURL, config) => {
     let demuxer = new TsDemuxerJs.TsDemuxerJsMod(videoURL, config);
     return demuxer;
