132350476ef3a04f4eca47c630f78414cd8d2433
numberwolf <porschegt23@foxmail.com>
2020-08-30 23:00:34 +0800

    addMpegTSSupport

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const TsDemuxerJs = require('./tsdemuxerjs')
+const TsDemuxerJs = require('./mpeg');
 global.makeTsDemuxerJs = (videoURL, config) => {
     let demuxer = new TsDemuxerJs.TsDemuxerJsMod(videoURL, config);
     return demuxer;
